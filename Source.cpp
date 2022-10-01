643f4dc1d2053ed4dc6dea4ce1b190d4efd13a52
saugat1794 <88509096+saugat1794@users.noreply.github.com>
2022-10-01 13:06:09 -0500

    Data Exploration C++

--- Source.cpp
+++ Source.cpp
@@ -1,287 +1,144 @@
-#include<iostream>
-#include<fstream>
-#include<String>
-#include<vector>
-#include<sstream>
-#include<algorithm>
-#include<Eigen/Dense>
-#include<chrono>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
 
-
-using namespace Eigen;
 using namespace std;
-using namespace chrono;
 
-const int MAX_LEN = 1050;
-vector<string> sex;
-vector<string> survived;
-void setValue(string);
-double alpha = 0.001;
-double e = 2.71828;
-int epoch = 10;
-MatrixXd weight(2, 1);
-
-int read_data(string);
-void setvalue(string);
-void display(vector<string>);
-MatrixXd set_datamatrix(vector<string>);
-MatrixXd set_weight();
-MatrixXd multiply_matrix(MatrixXd, MatrixXd);
-MatrixXd train_data();
-vector<int> test_data();
-MatrixXd set_testData();
-double checkAccuracy(vector<int>);
-double checkSensitivity(vector<int>);
-double checkSpecificity(vector<int>);
-int numberofIteration = 52;
+double sum(vector<double>);
+double mean(vector<double>);
+double median(vector<double>);
+vector<double> range(vector<double>);
+double covariance(vector<double>, vector<double>);
+double correlation(vector<double>, vector<double>);
+double calculateStandardDeviation(vector<double>);
+void display(vector<double>);
 
 int main() {
-    
-	read_data("titanic_project.csv");
-    auto start = steady_clock::now();
-    MatrixXd final_weight = train_data();
-    auto end = steady_clock::now();
-    duration<double> elapsed_time = end - start;
-    cout << "********************************************" << endl;
-    cout << "             Coefficients                   " << endl;
-    cout << "********************************************" << endl;
-    cout << "w0 = " << final_weight(0) << endl;
-    cout << "w1 = " << final_weight(1) << endl;
-    cout << "********************************************" << endl;
-    cout << "Required equation: " << endl;
-    cout << "Survived = " << final_weight(0) << " + " << final_weight(1) << " age " << endl;
-    cout << "********************************************" << endl;
-
-    vector<int> result = test_data();
-    cout << "********************************************" << endl;
-    cout << "                  Metrics                   " << endl;
-    cout << "********************************************" << endl;
-    cout << "Accuracy = " << checkAccuracy(result) << endl;
-    cout << "Sensitivity = " << checkSensitivity(result) << endl;
-    cout << "Specificity = " << checkSpecificity(result) << endl;
-
-    cout << "********************************************" << endl;
-    cout << "Number of run times = " << numberofIteration << endl;
-    cout << "********************************************" << endl;
-    
-    cout << "********************************************" << endl;
-    cout << "elapsed time for training data: " << elapsed_time.count() << " second " << endl;
-    cout << "********************************************" << endl;
-    //cout << "elapsed time: " << elapsed_time.count() << "s\n";
-    return 0;
-}
-
-int read_data(string filename) {
-    fstream inFS;
+    ifstream inFS;
     string line;
-   // string sex_temp, survived_temp;
-    vector<string> lineAll;
+    string rm_in, medv_in;
+    const int MAX_LEN = 1000;
+    vector<double> rm(MAX_LEN);
+    vector<double> medv(MAX_LEN);
+    
 
-    std::cout << "Opening file " << filename << endl;
+    cout << "Opening file Boston.csv" << endl;
 
-    inFS.open(filename);
+    inFS.open("Boston.csv");
 
     if (!inFS.is_open()) {
-        std::cout << "Could not open a file" << endl;
+        cout << "Could not open file" << endl;
         return 1;
     }
 
-    //std::cout << "Reading line 1" << endl;
-    std::getline(inFS, line);
-    //std::cout << "Heading " << line << endl;
+    cout << "Reading line 1" << endl;
+    getline(inFS, line);
 
+    cout << "Heading " << line << endl;
+
+    int numberofObservation = 0;
     while (inFS.good()) {
-        std::getline(inFS, line);
-        setValue(line);
+        getline(inFS, rm_in, ',');
+        getline(inFS, medv_in, '\n');
+        rm.at(numberofObservation) = stof(rm_in);
+        medv.at(numberofObservation) = stof(medv_in);
+
+        numberofObservation++;
+    }
+    rm.resize(numberofObservation);
+    medv.resize(numberofObservation);
+
+    cout << "New Line Size = " << numberofObservation << endl;
+
+    cout << "Closing file Boston.csv" << endl;
+    inFS.close();
+
+    cout << "******************** Stats of rm**************************" << endl;
+    display(rm);
+    cout << "************************************************************" << endl << endl;
+
+    cout << "******************** Stats of medv**************************" << endl;
+    display(medv);
+    cout << "************************************************************" << endl << endl;
+
+    cout << "Covariance = " << covariance(rm, medv) << endl;
+
+    cout << "\nCorrelation = " << correlation(rm, medv) << endl;
+
+    cout << "\n Program terminated." << endl;
+
+    return 0;
+}
+
+double sum(vector<double> temp) {
+    double sum = 0.0;
+    for (int i = 0; i < temp.size(); i++) {
+        sum += temp.at(i);
+    }
+    return sum;
+}
+
+double mean(vector<double> temp) {
+    double avg = 0.0;
+    int observation = temp.size();
+    double sumofVector = sum(temp);
+    return (double)sumofVector / (double)observation;
+
+}
+
+double median(vector<double> temp) {
+    double median = 0.0;
+    int size = temp.size();
+    sort(temp.begin(), temp.end());
+    if (size % 2 == 0) {
+        return (double)(temp.at((size - 1) / 2) + temp.at(size / 2)) / 2.0;
+    }else{
+        return (double)temp.at(size / 2);
     }
 }
 
-void setValue(string line) {
-    // survived @ 2nd column, and sex @ 3rd column. Started from index 0.
-    stringstream s_stream(line);
-    vector<string> result;
-    while (s_stream.good()) {
-        string substr;
-        getline(s_stream, substr, ','); //get first string delimited by comma
-        result.push_back(substr);
+vector<double> range(vector<double> temp) {
+    vector<double> result;
+    double left = (double)*min_element(temp.begin(), temp.end());
+    result.push_back(left);
+    double right = (double)*max_element(temp.begin(), temp.end());
+    result.push_back(right);
+    return result;
+}
+
+double covariance(vector<double> vect1, vector<double> vect2) {
+    double sum = 0;
+    double x_mean = mean(vect1);
+    double y_mean = mean(vect2);
+
+    for (int i = 0; i < vect1.size(); i++) {
+        sum = (double)sum + (vect1.at(i) - x_mean)*(vect2.at(i) - y_mean)/(double)(vect1.size());
     }
-    sex.push_back(result.at(3));
-    survived.push_back(result.at(2));
-
+    return sum;
 }
 
-double signoid(int z) {
-    return double(1/(1 + pow(e, -z)));
+double correlation(vector<double> vect1, vector<double> vect2) {
+    double standard_deviation_X = calculateStandardDeviation(vect1);
+    double standard_deviation_Y = calculateStandardDeviation(vect2);
+    return (double)covariance(vect1, vect2) / (double)(standard_deviation_X * standard_deviation_Y);
 }
 
-MatrixXd set_weight() {
-    weight.setOnes();
-    return weight;
-}
-
-MatrixXd set_datamatrix(vector<string> sex1) {
-    MatrixXd data_matrix(800,2);
-    data_matrix.col(0).setOnes();
-    
-    for (int i = 0; i < 800; i++) {
-        double data = stod(sex1.at(i));
-        data_matrix(i, 1) = data;
+double calculateStandardDeviation(vector<double> temp) {
+    double standardDeviation = 0.0;
+    for (int i = 0; i < temp.size(); i++) {
+        standardDeviation += pow(temp.at(i) - mean(temp), 2);
     }
-
-   // cout << data_matrix << endl;
-    return data_matrix;
+    return (double)sqrt(standardDeviation / temp.size());
 }
 
-
-MatrixXd multiply_matrix_for_train(MatrixXd data_matrix, MatrixXd weight) {
-    //MatrixXd data_matrix = set_datamatrix(sex);
-    //weight = set_weight();
-    MatrixXd prod = data_matrix * weight;
-    MatrixXd temp = prod.unaryExpr(&signoid);
-    return temp;
+void display(vector<double>temp) {
+   
+    cout << "Sum = " << sum(temp) << endl;
+    cout << "Mean = " << mean(temp) << endl;
+    cout << "Median = " << median(temp) << endl;
+    vector<double> rangeofVector = range(temp);
+    cout << "Range = " << rangeofVector.at(0) << " to " << rangeofVector.at(1) << endl;
+   
 }
-
-MatrixXd multiply_matrix_for_test(MatrixXd test, MatrixXd wt) {
-    MatrixXd prod = test * weight;
-    MatrixXd temp = prod.unaryExpr(&signoid);
-    return temp;
-}
-
-MatrixXd convert_label_train_data(vector<string>survived1) {
-    MatrixXd label(800,1);
-    for (int i = 0; i < 800; i++) {
-        double data = stod(survived1.at(i));
-        label(i) = data;
-    }
-    cout << "Training Data rows = " << label.rows() << endl;
-    return label;
-}
-
-MatrixXd convert_label_test_data(vector<string>survived1) {
-    MatrixXd label(246, 1);
-    int testData = 246;
-    int count = 0;
-    int start = 800;
-    for (int i = 0; i < testData; i++) {
-        double data = stod(survived1.at(start));
-        label(i) = data;
-        count++;
-        start++;
-    }
-    //cout << "Test label count = " << endl;
-    //cout << "Test Label rows = " << label.rows() << endl;
-
-    return label;
-}
-
-MatrixXd set_testData() {
-    int totalSize = sex.size();
-    int trainingData = 800;
-    int testData = totalSize - trainingData;
-    int count = 0;
-    MatrixXd testMatrix(246,2);
-    //cout << testMatrix.rows() << endl;
-
-    testMatrix.col(0).setOnes();
-    for (int i = 0; i < testData; i++) {
-        double data = stod(sex.at(trainingData));
-        count++;
-        testMatrix(i, 1) = data;
-        trainingData++;
-    }
-    return testMatrix;
-}
-
-vector<int> test_data() {
-    //MatrixXd wt = train_data();
-    int truePositive = 0;
-    int trueNegative = 0;
-    int falsePositive = 0;
-    int falseNegative = 0;
-    vector<double> storing;
-    vector<int> finalRes;
-    vector<pair<double, double>> tempRes;
-    MatrixXd testMatrix = set_testData();
-    MatrixXd label = convert_label_test_data(survived);
-    MatrixXd pred1 = multiply_matrix_for_test(testMatrix, weight);
-    
-    int start = 800;
-    for (int i = 0; i < pred1.rows(); i++) {
-        if (pred1.coeff(i) < 0.5) {
-            storing.push_back(0);
-        }
-        else {
-            storing.push_back(1);
-        }
-    }
-    
-    for (int i = 0; i < storing.size(); i++) {
-        tempRes.push_back(make_pair(storing.at(i), stod(survived.at(start))));
-        start++;
-    }
-
-    //cout << tempRes.size() << endl;
-    for (int i = 0; i < tempRes.size(); i++) {
-        //cout << tempRes.at(i).first << "-------" << tempRes.at(i).second << endl;
-        if (tempRes.at(i).first == 1 && tempRes.at(i).second == 1) {
-            truePositive++;
-        }
-        else if (tempRes.at(i).first == 0 && tempRes.at(i).second == 0) {
-            trueNegative++;
-        }
-        else if (tempRes.at(i).first == 1 && tempRes.at(i).second == 0) {
-            falsePositive++;
-        }
-        else if (tempRes.at(i).first == 0 && tempRes.at(i).second == 1) {
-            falseNegative++;
-        }
-    }
-    /*
-    cout << "TP = " << truePositive << endl;
-    cout << "TN = " << trueNegative << endl;
-    cout << "FP = " << falsePositive << endl;
-    cout << "FN = " << falseNegative << endl;
-    
-    */
-    
-    finalRes.push_back(truePositive);
-    finalRes.push_back(trueNegative);
-    finalRes.push_back(falsePositive);
-    finalRes.push_back(falseNegative);
-    return finalRes;
-}
-
-MatrixXd train_data() {
-    weight = set_weight();
-    MatrixXd label = convert_label_train_data(survived);
-    for (int i = 0; i < 52; i++) {
-        MatrixXd setTemp = set_datamatrix(sex);
-        MatrixXd prob = multiply_matrix_for_train(setTemp,weight);
-        MatrixXd error = label - prob;
-        weight = weight + alpha * setTemp.transpose() * error;
-    }
-    return weight;
-}
-
-/*
-res[0] = truePositive;
-res[1] = trueNegative;
-res[2] = falsePositive;
-res[3] = falseNegative;
-*/
-
-double checkAccuracy(vector<int> res) {
-    return double(double(res.at(0) + res.at(1)) / double(res.at(0) + res.at(1) + res.at(2) + res.at(3)) * 100);
-}
-
-double checkSensitivity(vector<int> res) {
-    return double(double(res.at(0)) / double(res.at(0) + res.at(3)) * 100);
-}
-
-double checkSpecificity(vector<int> res) {
-    return double(double(res.at(1)) / double(res.at(1) + res.at(2)) * 100);
-}
-
-
-
-
